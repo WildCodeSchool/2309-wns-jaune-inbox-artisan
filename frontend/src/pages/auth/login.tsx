48b3d0329575b203ff63b886dc77e1b295904090
jdhaisne <julien.dhaisne@gmail.com>
2024-02-09 10:07:38 +0100

    started login

--- frontend/src/pages/auth/login.tsx
+++ frontend/src/pages/auth/login.tsx
@@ -1,4 +1,8 @@
-function Login() {
+import { ReactElement } from "react";
+import { NextPageWithLayout } from "../_app";
+import LoginLayout from "@/components/layout-elements/LoginLayout";
+
+const  Login: NextPageWithLayout =() => {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24`}
@@ -6,6 +10,15 @@ function Login() {
       Ici le formulaire de login
     </main>
   );
+}
+
+
+Login.getLayout = function getLayout(page: ReactElement) {
+	return (
+		<LoginLayout title='login' fullWidthImage >
+			{page}
+		</LoginLayout>
+	)
 }
 
 export default Login;
