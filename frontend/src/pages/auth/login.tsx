f18ac9881e7a28e6896f44eada1386d61a580d5d
jdhaisne <julien.dhaisne@gmail.com>
2024-03-15 09:45:20 +0100

    cleaning after login merge

--- frontend/src/pages/auth/login.tsx
+++ frontend/src/pages/auth/login.tsx
@@ -55,54 +55,6 @@ const  Login: NextPageWithLayout = () => {
   );
 }
 
-const Login: NextPageWithLayout = () => {
-	const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
-		console.log('Success:', values);
-	};
-
-	const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (
-		errorInfo
-	) => {
-		console.log('Failed:', errorInfo);
-	};
-	return (
-		<main className={`flex flex-col items-center justify-between p-24`}>
-			<Form
-				name="basic"
-				labelCol={{ span: 12 }}
-				wrapperCol={{ span: 16 }}
-				style={{ maxWidth: 600 }}
-				initialValues={{ remember: true }}
-				onFinish={onFinish}
-				onFinishFailed={onFinishFailed}
-				autoComplete="off"
-			>
-				<Form.Item<FieldType>
-					label="Mail"
-					name="mail"
-					rules={[{ required: true, message: 'Please input your mail!' }]}
-				>
-					<Input />
-				</Form.Item>
-
-				<Form.Item<FieldType>
-					label="Password"
-					name="password"
-					rules={[{ required: true, message: 'Please input your password!' }]}
-				>
-					<Input.Password />
-				</Form.Item>
-
-				<Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-					<Button type="primary" htmlType="submit">
-						Submit
-					</Button>
-				</Form.Item>
-			</Form>
-		</main>
-	);
-};
-
 Login.getLayout = function getLayout(page: ReactElement) {
 	return (
 		<LoginLayout title='Good to see you !' subtitle="Drag and drop is addictive isn’t it ?" fullWidthImage >
